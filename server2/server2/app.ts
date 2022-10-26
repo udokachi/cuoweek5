6d1ccb811a427573929189fbe9364a89fc46dabd
mac <mac@chidinmas-MacBook-Air.local>
2022-10-26 16:21:55 +0100

    task

--- server2/server2/app.ts
+++ server2/server2/app.ts
@@ -1,14 +1,15 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
+import { parser } from "html-metadata-parser"
 /*
 implement your server code here
 */
-
-const server: Server = http.createServer(
-  (req: IncomingMessage, res: ServerResponse) => {
-    if (req.method === "GET") {
-      res.end(JSON.stringify({ name: "hello" }));
+const server: Server = http.createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    if (req.url ==="/" && req.method === "GET") {
+      const result = await parser("https://www.facebook.com/");
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(JSON.stringify(result, null,2));
+      console.log(JSON.stringify(result, null,2))
     }
   }
 );
-
-server.listen(3001);
+server.listen(6706);
